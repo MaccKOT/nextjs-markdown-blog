e2a8c05a127c418e266d8d0c0ac9df877b90efda
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2021-06-29 08:39:06 +0400

    Post component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import Head from 'next/head';
+import Post from './components/Post';
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div>
       <Head>
@@ -9,15 +13,41 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main>
-        <h1>
-          Welcome to <a href='https://nextjs.org'>Next.js!</a>
-        </h1>
-
-        <p>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+      <div className='posts'>
+        {posts.map((post, index) => (
+          <Post post={post} />
+        ))}
+      </div>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  // Get files from the posts directory
+  const files = fs.readdirSync(path.join('posts'));
+
+  //Get slug and frontmatter from posts
+  const posts = files.map((filename) => {
+    //create slug
+    const slug = filename.replace('.md', '');
+    //create front matter
+    const markdownWithMeta = fs.readFileSync(
+      path.join('posts', filename),
+      'utf-8'
+    );
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  // console.log(posts);
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
